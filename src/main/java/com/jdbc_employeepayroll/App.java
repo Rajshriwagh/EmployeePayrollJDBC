aa694bb898eed1b2a1d0e9827045045fce6c8fbc
Rajshri Wagh <mahajan.rajshri16@gmail.com>
2023-01-04 20:31:59 +0530

    Add: Initial commit

---
+++ src/main/java/com/jdbc_employeepayroll/App.java
@@ -0,0 +1,13 @@
+package com.jdbc_employeepayroll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to EmployeePayroll JDBC!" );
+    }
+}
